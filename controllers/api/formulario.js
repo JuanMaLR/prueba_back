8c8560f75213efae993471f620b6d98efc757049
JuanMaLR <A01206568@itesm.mx>
2022-08-23 11:29:35 -0500

    Getting more data 2

--- controllers/api/formulario.js
+++ controllers/api/formulario.js
@@ -1,8 +1,7 @@
 const formularioController = {
     showData: (req, res) => {
         console.log("Recibi algo");
-        console.log(res);
-        return res.status(200).json({ data: "Hola, aquí estoy" });
+        res.status(200).json({ data: "Hola, aquí estoy" });
 	},
 };
 
